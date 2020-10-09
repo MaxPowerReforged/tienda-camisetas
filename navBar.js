74e83cf5ae56cd11dec91873ff43d392eb33fa10
MaxPowerEdain <joancarazo1994@gmail.com>
2020-10-09 08:03:43 +0200

    fixed resizing bug

--- navBar.js
+++ navBar.js
@@ -1,15 +1,26 @@
 'use strict'
+let isToggleVisible = true;
+let menuCollapse = document.querySelector("#menu-collapse");
 
 document.querySelector("#mobile-toggle").addEventListener('click', function(event){
     event.preventDefault();
-    console.log("toggling");
-    let menuCollapse = document.querySelector("#menu-collapse");
-    if (menuCollapse.style.display == "none") menuCollapse.style.display = "flex";
-    else menuCollapse.style.display = "none";
+    console.log("toggling: ", menuCollapse.style.display);
+    if (menuCollapse.style.display === "none" || menuCollapse.style.display === "") {
+        menuCollapse.style.display = "flex";
+        isToggleVisible = true;
+    }
+    else {
+        menuCollapse.style.display = "none";
+        isToggleVisible = false;
+    }
 });
 
 window.onresize = function(){
     if(window.innerWidth > 600){
-        document.querySelector("#menu-collapse").style.display = "flex";
+        menuCollapse.style.display = "flex";
+        isToggleVisible = false;
+    }
+    else if(window.innerWidth <= 600 && isToggleVisible === false) {
+        menuCollapse.style.display = "none";
     }
 }
